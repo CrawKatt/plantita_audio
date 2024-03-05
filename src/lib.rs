e3e917cd5b5c4daa6752dbe93d700a6e45417d29
crawkatt <man849916@gmail.com>
2024-03-05 15:42:45 -0300

    Corrigiendo bugs en la librería y añadiendo `Cargo.lock` al `.gitignore`

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 use std::io::{Read, Write};
 
 /// Convertir el archivo recibido a .mp3
-pub fn convert_to_mp3(input_path: &str, output_path: &str) -> std::io::Result<()> {
+pub fn convert_to_mp3(input_path: &str, output_path: &str) -> std::io::Result<String> {
     // Open the input file
     let mut input_file = File::open(input_path)?;
 
@@ -29,12 +29,12 @@ pub fn convert_to_mp3(input_path: &str, output_path: &str) -> std::io::Result<()
         .arg("2")
         .arg("-b:a")
         .arg("192k")
-        .arg(output_path)
+        .arg(&output_path)
         .output()?;
 
     if !output.status.success() {
         eprintln!("ffmpeg failed: {}", String::from_utf8_lossy(&output.stderr));
     }
 
-    Ok(())
+    Ok(output_path)
 }
